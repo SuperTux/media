b002651657f6468f92c0db72121212bd2378d0f5
Matthias Braun <matze@braunis.de>
2006-04-19 16:39:21 +0000

    imported grumbels xcf files from pingus.seul.org/~grumbel/tmp/supertux-sprite
    SVN-Revision: 3371

---
+++ images/xcf/grumbel/bonusvin.gap
@@ -0,0 +1,3 @@
+# GIMP / GAP Videoinfo file
+(framerate 1) # 1.0 upto 100.0 frames per sec
+(timezoom 1) # 1 upto 100 frames
---
+++ images/xcf/grumbel/sprite2/frame.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mpeg_encode /home/ingo/projects/supertux/images/sprite/frame.par_mpg
+echo 'mpeg_encode done.'
+read DUMMY
