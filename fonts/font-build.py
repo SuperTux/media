337519743f23971a76f62da09cf335d4404ef3c5
Ingo Ruhnke <grumbel@gmail.com>
2018-09-06 21:05:15 +0200

    Give SuperTux font a proper name and adjust spacing

--- fonts/font-build.py
+++ fonts/font-build.py
@@ -5,7 +5,16 @@ import sys
 import fontforge
 
 font = fontforge.font()
+
 font.encoding = "unicode"
+
+font.familyname = "SuperTux"
+font.fullname = "SuperTux Medium"
+font.fontname = "SuperTux Medium"
+
+glyph = font.createChar(ord(" "))
+glyph.width = 500
+
 for filename in os.listdir("glyphs"):
     idx = int(filename[0:4], 16)
     print("processing {} -> {}".format(filename, idx))
@@ -13,8 +22,9 @@ for filename in os.listdir("glyphs"):
     glyph.importOutlines(os.path.join("glyphs", filename))
     font.selection.select(idx)
     font.autoTrace()
+    font.autoWidth(200)
 
-font.generate("/tmp/out.otf")
-font.save("/tmp/out.sfd")
+font.generate("/tmp/SuperTux-Medium.otf")
+font.save("/tmp/SuperTux-Medium.sfd")
 
 # EOF #
