06a3fbd76b0823bf096aa71abcd97d2327c13966
Ingo Ruhnke <grumbel@gmail.com>
2018-12-22 20:34:54 +0100

    Add Noto base, Arabic and Hebrew to SuperTux font-build.py

--- fonts/font-build.py
+++ fonts/font-build.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python2
 
+# SuperTux Font Build Script
+# Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import sys
 import fontforge
+import psMat;
 
 font = fontforge.font()
 
 font.encoding = "unicode"
 
 font.familyname = "SuperTux"
-font.fullname = "SuperTux Medium"
-font.fontname = "SuperTux Medium"
+font.fullname = "SuperTux-Medium"
+font.fontname = "SuperTux-Medium"
 
 glyph = font.createChar(ord(" "))
 glyph.width = 500
@@ -24,6 +41,13 @@ for filename in os.listdir("glyphs"):
     font.selection.select(idx)
     font.autoTrace()
     font.autoWidth(200)
+    # font.autoHint()
+
+# for glyph in font.glyphs():
+#     glyph.transform(psMat.translate(0, 30))
+
+# font.ascent = 750
+# font.descent = 150
 
 # Set digits to fixed width
 
@@ -52,10 +76,22 @@ for c in "0123456789":
     # rounding issues and unequal width
     glyph.width = digit_width
 
-# font.mergeFonts("")
+# The next step requires the Google Noto fonts extracted into the noto/
+# directory, they can be found at:
+# https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip
 
-font.generate("/tmp/SuperTux-Medium.otf")
-font.generate("/tmp/SuperTux-Medium.ttf")
-font.save("/tmp/SuperTux-Medium.sfd")
+font.mergeFonts("noto/NotoSans-Regular.ttf")
+font.mergeFonts("noto/NotoSansArabic-Regular.ttf")
+font.mergeFonts("noto/NotoSansHebrew-Regular.ttf")
+
+# These make fontforge crash
+# font.mergeFonts("noto/NotoSansCJKjp-Regular.otf")
+# font.mergeFonts("noto/NotoSansCJKkr-Regular.otf")
+
+if not os.path.exists("build"):
+    os.mkdir("build")
+font.generate("build/SuperTux-Medium.otf")
+font.generate("build/SuperTux-Medium.ttf")
+font.save("build/SuperTux-Medium.sfd")
 
 # EOF #
